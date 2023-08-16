be23182378dc2be23571294a2ef8ba0bd088fb6a
Tetoai <kennethquarku@gmail.com>
2023-08-15 18:28:54 -0700

    Nature made the natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Function to sum multiples of 3 or 5 in numbersbelow 1024
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + 1;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
