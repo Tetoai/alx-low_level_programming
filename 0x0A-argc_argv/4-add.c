a44c1e4eb5aedcd8e686200593b32a48e6e5e968
root <you@example.com>
2023-09-06 12:06:31 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,9 +4,9 @@
 #include <string.h>
 	/**
 	 * main - Entry point
-	 * check_num - check string there are digit
-	 * @str: array str
-	 * Return: Always 0 (Success)
+	 * check_num - Check if the input string contains only digits.
+	 * @str: The input string to check.
+	 * Return: 1 if the string contains only digits, 0 otherwise.
 	 */
 	int check_num(char *str)
 	{
@@ -25,32 +25,27 @@
 		return (1);
 	}
 
-
 	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 * Return: Always 0 (Success)
+	 * main - Entry point of the program, calculates the sum of numbers in the command-line arguments.
+	 * @argc: he number of command-line arguments.
+	 * @argv: An array of strings containing the command-line arguments.
+	 * Return: 0 on success, 1 if there is an error (e.g., non-digit characters in arguments).
 	 */
 
 	int main(int argc, char *argv[])
 	{
-
 		/*Declaring variables*/
 		int count;
 		int str_to_int;
 		int sum = 0;
-
 		count = 1;
 		while (count < argc) /*Goes through the whole array*/
 		{
 			if (check_num(argv[count]))
-
 			{
 				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 				sum += str_to_int;
 			}
-
 			/*Condition if one of the number contains symbols that are not digits*/
 			else
 			{
@@ -59,8 +54,6 @@
 			}
 			count++;
 		}
-
 		printf("%d\n", sum); /*print sum*/
-
 		return (0);
 	}
